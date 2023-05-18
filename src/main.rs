da618f3a73e57f1076424ff219a04e891d9590fe
develop-build <deepak7business@gmail.com>
2023-05-18 20:36:23 +0530

    covered structs  tuples

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@
 // mod conditionals;
 // mod loops;
 // mod functions;
-mod pointer_ref;
+// mod pointer_ref;
+mod structs;
 
 fn main() {
     // main::run();
@@ -18,5 +19,6 @@ fn main() {
     // conditionals::run();
     // loops::run();
     // functions::run();
-    pointer_ref::run();
+    // pointer_ref::run();
+    structs::run();
 }
